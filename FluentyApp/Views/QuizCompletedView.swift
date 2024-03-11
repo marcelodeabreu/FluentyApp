b945218487cad81b35224daae041e7684532cfff
Marcelo de Abreu <109244891+marcelodeabreu@users.noreply.github.com>
2024-03-11 17:50:53 -0300

    Add files via upload
    Button play again with reset func at QuizCompletedView

--- FluentyApp/Views/QuizCompletedView.swift
+++ FluentyApp/Views/QuizCompletedView.swift
@@ -15,6 +15,22 @@ struct QuizCompletedView: View {
         VStack {
             
             Text("GAME COMPLETED")
+                .font(.title2)
+            
+            Button {
+                            gameManagerVM.restartGame()
+                        } label: {
+                            HStack {
+                                Text("Jogar Novamente")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 24, weight: .bold, design: .rounded))
+                            }
+                        }.frame(width: 300, height: 60, alignment: .center)
+                            .background(.green.opacity(0.7))
+                            .cornerRadius(30)
+                            .padding()
+                        
+                    
             
         }
     }
